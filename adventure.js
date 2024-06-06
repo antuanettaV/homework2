c067d9508f3a5838034f237607e4696370d8c177
Hanna Vorontsova <159482282+antuanettaV@users.noreply.github.com>
2024-06-06 06:39:55 +0300

    Add existing file

---
+++ adventure.js
@@ -0,0 +1,29 @@
+const yearOfBirth=1987;
+const yearFuture=2027;
+console.log(yearFuture-yearOfBirth);
+const message="You will be 40 years old in 2027";
+console.log(message);
+const dogYearOfBirth=1957;
+const dogYearFuture=2027;
+const dogYear=dogYearFuture-dogYearOfBirth;
+if (dogYear>50){
+console.log ('Your dog will be 10 human years old in 2027');
+} else if (dogYear=50) {
+console.log ('Your dog will be 70 dog years old in 2027');
+}   
+var height1=10;
+var deep1=10;
+var wide1=8;
+var gardensize1=100;
+console.log(height1*deep1*wide1*2.5*1000+gardensize1*300);
+var height2=8;
+var deep2=11;
+var wide2=5;
+var gardensize2=70;
+console.log(height2*deep2*wide2*2.5*1000+gardensize2*300);
+const firstWords = ["Easy", "Corporate","Awesome", "Phenomenal", "Spectacular", "Unbelievable", "Splendid", "Significant", "Startling", "Wonderful"];
+const secondWords = ["Team", "Company","Corporation", "Firm", "Proposition", "Opportunity", "Decision", "Solution", "Help", "Idea"];
+const randomName = Math.floor(Math.random() * 10);
+console.log(randomName);
+const startupName= 'The startup:"Spledid Decision contains 15 characters";'
+console.log(startupName);
